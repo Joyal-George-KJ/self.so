cd5d79451d5f034f14453b4b51c42d37f3c6b705
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-08-04 20:54:23 +0530

    Added: Custom Theme Toggle Hook

---
+++ hooks/useTheme.ts
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+type Theme = 'dark' | 'light';
+
+const useTheme = () => {
+  const [theme, setTheme] = useState<Theme>('dark');
+  const [mount, setMount] = useState(false);
+
+  const toggleTheme: (passedTheme?: Theme, toggle?: boolean) => void = (passedTheme: Theme = theme, toggle = false) => {
+    if (typeof window !== 'undefined') {
+      // initiateTheme function will initiate all the value to local storage and state
+      const initiateTheme = (currentTheme: Theme) => {
+        localStorage.setItem('theme', currentTheme);
+        setTheme(currentTheme);
+        document.documentElement.classList.toggle(
+          'dark',
+          currentTheme === 'dark'
+        );
+      };
+      // if window exist follow through;
+      if (localStorage.getItem('theme') === null) {
+        // local storage has nothing but null fetch device theme and initiate the change
+        let deviceTheme: Theme = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        ).matches
+          ? 'dark'
+          : 'light';
+        initiateTheme(deviceTheme);
+      } else if (localStorage.getItem('theme') && toggle) {
+        // local storage has value and prop has toggle as true then toggle it
+        let currentTheme: Theme = passedTheme === 'dark' ? 'light' : 'dark';
+        initiateTheme(currentTheme);
+      } else if (localStorage.getItem('theme') && !toggle) {
+        // local storage has value and prop has toggle as false then don't toggle it
+        let currentTheme = localStorage.getItem('theme') as Theme;
+        initiateTheme(currentTheme);
+      }
+    }
+  };
+
+  useEffect(() => {
+    // on component mount call the toggle function without toggle true prop it will initialize the theme and sets it
+    toggleTheme();
+    setMount(true);
+  }, []);
+
+  return {mount: mount, theme: theme, toggleTheme: toggleTheme};
+};
+
+export default useTheme;
